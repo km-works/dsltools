a65550a1c499220461057d6b157cceb9c4e422f4
Christian P. Lerch <christian.p.lerch@gmail.com>
2018-03-23 18:08:50 +0100

    minor reformat

--- dsltools-common/src/main/java/kmworks/dsltools/adt/adt/ADT.java
+++ dsltools-common/src/main/java/kmworks/dsltools/adt/adt/ADT.java
@@ -9,7 +9,14 @@ import org.immutables.value.Value;
  */
 @Value.Immutable
 public abstract class ADT extends AbstractRoot {
-    @Value.Parameter public abstract String name();
-    @Value.Parameter public abstract Seq<TypeDef> main();
-    @Value.Parameter public abstract Seq<TypeDef> aux();
+
+    @Value.Parameter
+    public abstract String name();
+
+    @Value.Parameter
+    public abstract Seq<TypeDef> main();
+
+    @Value.Parameter
+    public abstract Seq<TypeDef> aux();
+
 }
